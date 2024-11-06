fd851b37dbf71f9fc7fbd88bb7caac0df3eb7668
mika <mikasa2511592510@gmail.com>
2024-11-06 19:55:49 +0800

    chore(vscode): 添加 unistd.h 文件关联
    - 在 .vscode/settings.json 文件中添加了 "unistd.h": "c" 的文件关联
    - 这个改动有助于 VSCode 正确识别和处理 unistd.h 头文件

--- screen/main.c
+++ screen/main.c
@@ -15,5 +15,48 @@
 
 int main()
 {
+    int touch_fd = Init_touch();
+    int lcd_fd = init_lcd();
+    lcd_clear(BLACK_COLOR);
+    //把音乐的背景图片画出来
+    Bmp_show("/IOT/lab/music_background.bmp");//音乐背景图
+    printf("音频播放背景图片");
+    //开始播放音乐
+    music_loop("/IOT/lab/music/This World.mp3");
+    printf("音乐开始播放");
+    
+    printf("等待动作");
+    //等待动作
+    while(1)
+    {
+        //暂停
+        if(get_rectangle_button_state(touch_fd, 400, 200, 100, 50))
+        {
+            printf("暂停\n");
+            music_stop();
+            lcd_draw_word(400, 200, 100, 50,pause_text, RED_COLOR);
+        }
+        //播放
+        if(get_rectangle_button_state(touch_fd, 400, 250, 100, 50))
+        {
+            printf("播放\n");
+            music_continue();
+            lcd_draw_word(400, 250, 100, 50,char_play, RED_COLOR);
+        }
+        //下一曲
+        if(get_rectangle_button_state(touch_fd, 400, 300, 100, 50))
+        {
+            printf("下一曲\n");
+            music_cancel();
+            lcd_draw_word(400, 300, 100, 50,pre_song, RED_COLOR);
+        }
 
+        //上一曲
+        if(get_rectangle_button_state(touch_fd, 400, 350, 100, 50))
+        {
+            printf("上一曲\n");
+            music_cancel();
+            lcd_draw_word(400, 350, 100, 50,next_song, RED_COLOR);
+        }
+    }
 }
