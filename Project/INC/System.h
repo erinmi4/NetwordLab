1a242ae341461eb2f61b822300dfe83b325c9ef8
mika <mikasa2511592510@gmail.com>
2024-11-08 01:08:55 +0800

    feat(系统): 添加毫秒级延时功能
    - 新增 System.h 头文件，定义了 sleep_ms 函数原型
    - 实现 sleep_ms 函数，提供毫秒级别的延时功能
    - 使用 select 函数来实现延时，确保跨平台兼容性

---
+++ Project/INC/System.h
@@ -0,0 +1,12 @@
+//
+// Created by 25115 on 2024/11/8.
+//
+
+#ifndef PROJECT_SYSTEM_H
+#define PROJECT_SYSTEM_H
+#include <stdio.h>
+#include <sys/select.h>
+
+void sleep_ms(unsigned int secs);//完成毫秒级别的延时
+
+#endif //PROJECT_SYSTEM_H
