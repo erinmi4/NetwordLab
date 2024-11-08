b3c3c86a33ae013377a8d1313be3c43bccfff176
wind <czf031015003920@gmail.com>
2024-11-08 18:59:43 +0800

    feat(voice_project): 添加 ALSA 音频库的配置和接口头文件
    - 新增 asoundef.h, asoundlib.h, conf.h 和 alisp.h 头文件
    - 设置文件关联，将 lcd.h 和 voicectl.h 关联为 C 语言文件
    - 这些文件提供了 ALSA 音频库的配置接口和常量定义

---
+++ Project/voice_project/inc/touch.h
@@ -0,0 +1,16 @@
+#ifndef __TOUCH_H__
+#define __TOUCH_H__
+
+
+#define UP    1
+#define DWON  2
+#define RIGHT 3
+#define LEFT  4
+#define EXIT  5
+
+//能够获取滑动的方向
+int get_dir();
+
+
+
+#endif
---
+++ Project/voice_project/inc/uart.h
@@ -0,0 +1,13 @@
+#ifndef __UART_H__
+#define __UART_H__
+
+
+
+void init_tty(int fd);
+void gy39_getlux();
+void gy39_get_temp();
+void get_smoke();
+
+
+
+#endif
